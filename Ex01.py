6f1fcf311cbf875b6c144bf9f3099e0f0afba276
JohnnyUFT <johnnyuft@gmail.com>
2017-08-10 15:11:35 -0300

    Inicializando o projeto

---
+++ Ex01.py
@@ -0,0 +1,23 @@
+# !/usr/bin/python3.5
+# -*- coding: utf-8 -*-
+
+'''
+author: Johnny Pereira
+email: (eufrazius,johnnyuft)@gmail.com
+last modified: August 2017
+'''
+import PIL
+import tkinter as tk
+from tkinter import*
+import string
+from PIL import ImageTk, Image
+from tkinter import PhotoImage
+import sys
+from tkinter import messagebox
+from tkinter import ttk
+
+
+# *******************************
+# PRIMEIROS testes com biblioteca OpenGl
+# *******************************
+def principal():
