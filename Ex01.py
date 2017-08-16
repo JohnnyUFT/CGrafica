e3f87b3f044daed356ca352e6c38e9d2ad3c1ba9
JohnnyUFT <johnnyuft@gmail.com>
2017-08-16 20:21:06 -0300

    commit da hora

--- Ex01.py
+++ Ex01.py
@@ -1,23 +1,15 @@
-# !/usr/bin/python3.5
 # -*- coding: utf-8 -*-
 
 '''
 author: Johnny Pereira
-email: (eufrazius,johnnyuft)@gmail.com
+email: johnnyuft@gmail.com
 last modified: August 2017
 '''
-import PIL
-import tkinter as tk
-from tkinter import*
-import string
-from PIL import ImageTk, Image
-from tkinter import PhotoImage
-import sys
-from tkinter import messagebox
-from tkinter import ttk
-
+import OpenGL
+import py
 
 # *******************************
 # PRIMEIROS testes com biblioteca OpenGl
 # *******************************
-def principal():
+class principal():
+    print("Olá")
